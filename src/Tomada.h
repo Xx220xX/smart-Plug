e388373d9e2e4e18ea1c5252c69f7a394ef7b0c4
Xx220xX <hslhenriquegoscara@gmail.com>
2018-10-23 19:38:59 -0300

    Update Tomada.h

--- src/Tomada.h
+++ src/Tomada.h
@@ -1,71 +1,55 @@
-/** version beta 1.0.0001**/
-typedef  struct {
-    int dia,hora,min,seg;
-    long long int realT;
-}Tempo;
+/** version beta 1.0.0002**/
 
 
-typedef enum {
-    TEMPERATURA, HUMIDADE, TEMPO, EVERON, EVEROFF
-} Parametros;
 
-typedef struct {
-    int minHum, maxHum, minTemp, maxTemp;
-    Tempo tempo;
-    
-} DadosParametro;
-
-typedef struct {
-    float hum, tempCels, tempFar;
-    Tempo horaAtual;
-} DadosSensores;
 void arrumaTempo(Tempo *tmp);
-void manterAte(char *ativa,Tempo *tmpInicio,Tempo *tmpAtual,long long int fim);
 void manterEntre(char *ativa, int min, int max, float atual);
 void ativarTomada(char parametro, char *ativa, DadosParametro *myparamentros, DadosSensores *myDados, char sensorDesativado);
 
+void ativoAte(char *ativa, Tempo fim, Tempo horaAtual, Tempo t0);
 
 
-void arrumaTempo(Tempo *tmp){
-    tmp->realT = tmp->hora*60*60+tmp->min*60+tmp->seg+tmp->dia*24*60*60;
+void arrumaTempo(Tempo *tmp) {
+  tmp->realT = tmp->hora * 60 * 60 + tmp->minu * 60 + tmp->seg + tmp->dia * 24 * 60 * 60;
 }
-void manterAte(char *ativa,Tempo *tmpInicio,Tempo *tmpAtual,long long int fim){
-    arrumaTempo(tmpInicio);
-    arrumaTempo(tmpAtual);
-    if(tmpAtual->realT-tmpInicio->realT>fim){
-        *ativa=0;
-    }
-}
-
 
 void manterEntre(char *ativa, int min, int max, float atual) {
-    if (atual <= min) {
-        *ativa = 1;
-    } else if (atual >= max) {
-        *ativa = 0;
-    }
+  if (atual <= min) {
+    *ativa = 1;
+  } else if (atual >= max) {
+    *ativa = 0;
+  }
 }
 
-void ativarTomada(char parametro, char *ativa, DadosParametro *myparamentros, DadosSensores *myDados,long long int *tmpInicio, char sensorDesativado) {
-    if(!sensorDesativado){
-      switch (parametro) {
-          case TEMPERATURA:
-              manterEntre(ativa, myparamentros->minTemp, myparamentros->maxTemp, myDados->tempCels);
-              break;
-          case HUMIDADE:
-              manterEntre(ativa, myparamentros->minHum, myparamentros->maxHum, myDados->hum);
-              break;
-          case TEMPO:
-              manterAte(ativa, &myparamentros->tempo, &myDados->horaAtual,*tmpInicio);
-              break;
-          case EVERON:
-              *ativa = 1;
-              break;
-          case EVEROFF:
-              *ativa = 0;
-              break;
-      }
-    }else{
-      Serial.println("os sensores estao com defeito");
+void ativarTomada(char parametro, char *ativa, DadosParametro *myparamentros, DadosSensores *myDados,  char sensorDesativado) {
+  if (!sensorDesativado) {
+    switch (parametro) {
+      case TEMPERATURA:
+        manterEntre(ativa, myparamentros->minTemp, myparamentros->maxTemp, myDados->tempCels);
+
+        break;
+      case HUMIDADE:
+        manterEntre(ativa, myparamentros->minHum, myparamentros->maxHum, myDados->hum);
+        break;
+      case TEMPO:
+        ativoAte(ativa, myparamentros->tempo, myDados->horaAtual, t0);
+        break;
+      case EVERON:
+        *ativa = 1;
+        break;
+      case EVEROFF:
+        *ativa = 0;
+        break;
     }
+  } else {
+    Serial.println("os sensores estao com defeito");
+  }
+}
+
+void ativoAte(char *ativa, Tempo fim, Tempo horaAtual, Tempo t0) {
+  if (horaAtual.realT - t0.realT > fim.realT) {
+    *ativa = 0;
+  }else {
+    *ativa=1;
+  }
 }
